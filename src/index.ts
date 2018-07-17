59da2418f427bfa87504378c793317cf305e4bd4
James Chong <wei3hua2@gmail.com>
2018-07-17 17:23:36 +0800

    added documentation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
 import faker from 'faker';
 import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 
-/** Faker Module
- * @namespace Faker
+/** Faker
+ * @namespace Faker 
+ * 
+ * @description
+ * Generation of fake data
+ * 
+ * @example
+ * rps install faker
+ * 
+ * 
 */
 @RpsModule("faker")
 export default class RPSFaker {
 
+  /**
+ * @function fake
+ * @memberof Faker
+ * @example
+ * ;Generates fake last, first and suffix
+ * fake "{{name.lastName}}, {{name.firstName}} {{name.suffix}}"
+ * @param {string} text Generate fake data using mustache string format.
+ * @returns {*} Fake content.
+ * @summary Generate fake data
+ * @description Refer to the method Faker.fake() about the usage and the list of API methods
+ * 
+ * @see {@link https://www.npmjs.com/package/faker}
+ * 
+*/
   @rpsAction({verbName:'fake'})
   async fake (ctx:RpsContext,opts:Object, text:string) : Promise<string>{
     return faker.fake(text);
